a4096b02cb2cb4ce42ce6f0c6da14efca43e4b50
Ole Thomsen Buus <otbu.ipu@gmail.com>
2018-06-24 00:45:06 +0200

    Fixes to parser.

--- src/TcDUTParser.py
+++ src/TcDUTParser.py
@@ -1,60 +1,44 @@
 
-from xml.parsers import expat
+from xml.etree import ElementTree
+import re
 
-elements = dict(
-    root=dict(name='TcPlcObject',isFound=False),
-    dut=dict(name='DUT',isFound=False),
-    decl=dict(name='Declaration',isFound=False))
-
-dut_filename = r'../data/TestTypeA.TcDUT'
+dut_filename = r'../data/TestTypeB.TcDUT'
 
 elementName_object = 'TcPlcObject'
 elementName_dut = 'DUT'
 elementName_declaration = 'Declaration'
 
-elementIsFound_object = False
-elementIsFound_dut = False
-elementIsFound_declaration = False
-cached_cdata = ''
-
-def start_element(name, attributes):
-
-    global \
-        elements, \
-        elementIsFound_object, \
-        elementIsFound_dut, \
-        elementIsFound_declaration
-
-    if name == elements['root']['name']:
-        print('-- Found', elements['root']['name'])
-        elements['root']['isFound'] = True
-
-    if name == elements['dut']['name']:
-        print('-- Found', elements['dut']['name'])
-        elements['dut']['isFound'] = True
-
-    if name == elements['decl']['name']:
-        print('-- Found', elements['decl']['name'])
-        elements['decl']['isFound'] = True
-
-
-def char_data(data):
-    global cached_cdata
-    if elements['root']['isFound'] and elements['dut']['isFound'] and elements['decl']['isFound']:
-        cached_cdata += data
-
 def main():
-    #p = expat.ParserCreate()
-    #p.StartElementHandler = start_element
-    #p.CharacterDataHandler = char_data
-    #with open(r'../../TestTypeA.TcDUT', 'rb') as file:
-    #    p.ParseFile(file)
-    #print('-- Found DUT CharacterData:\n', repr(cached_cdata))
 
-    import xml.etree.ElementTree as ET
-    tree = ET.parse(dut_filename)
-    root = tree.getroot()
-    print(root[0][0].text)
+    tree = ElementTree.parse(dut_filename)
+    elem_root = tree.getroot()
+
+    attributes = dict()
+    attributes['object_version'] = elem_root.get('Version')
+    attributes['prod_version'] = elem_root.get('ProductVersion')
+    attributes['type_name'] = None
+    attributes['type_id'] = None
+    attributes['type_text'] = None
+
+    elem_dut = elem_root.find('DUT')
+    elem_decl = None
+
+    if elem_dut is not None:
+        elem_decl = elem_dut.find('Declaration')
+        attributes['type_name'] = elem_dut.get('Name')
+        attributes['type_id'] = elem_dut.get('Id')
+
+    if elem_decl is not None:
+        attributes['type_text'] = elem_decl.text
+
+    print(attributes)
+
+    m0 = re.search(r'(?s)TYPE\s+?(?P<name>\S+?)\s*?:(?P<body>.*?)END_TYPE', attributes['type_text'])
+    type_name = m0.group('name')
+    type_body = m0.group('body')
+    #print(type_body)
+    m1 = re.search(r'(?s)STRUCT(.*?)END_STRUCT', type_body)
+    print(m1.group(1))
 
 if __name__ == '__main__':
     main()
